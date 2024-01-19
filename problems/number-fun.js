c9256440dea83dd1849915f238bcac1ffb441805
kevin <kchase07@gmail.com>
2024-01-18 23:42:28 -0600

    2nd comm

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,11 +1,20 @@
 function returnsThree() {
-  // Your code here 
+  return 3;
 }
 
-function reciprocal(n) {
-  // Your code here 
+function reciprocal(n = 0) {
+  if(n < 1 || n > 1000000) {
+    throw new RangeError('Argument must be between 1 and 1000000')
+  }
+  return 1/n;
 }
-
+//  try {
+//   reciprocal(5);
+//   reciprocal(-10);
+//   reciprocal(2000000);
+// } catch (error) {
+//   console.log(error.message)
+// }
 module.exports = {
   returnsThree,
   reciprocal
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,30 @@
-// Your code here 
+// Your code here
+const { expect } = require('chai');
+const { returnsThree, reciprocal } = require('../problems/number-fun')
+
+describe ('returnsThree', () => {
+    it('function returns 3', () => {
+        const res = returnsThree();
+        expect(res).to.equal(3)
+    })
+})
+
+describe ('reciprocal', () => {
+    context('with valid arguments', () => {
+    it('should return the reciprocal of n', () => {
+        const input = 4;
+        const res = reciprocal(input);
+        expect(res).to.equal(1/4);
+        expect(res).to.not.equal(!(1/4));
+    })
+})
+context('with invalid arguments', () => {
+    it('should throw a RangeError for arguments less than 1', () => {
+      expect(() => reciprocal(0)).to.throw(RangeError, "Argument must be between 1 and 1000000");
+    });
+
+    it("should throw a RangeError for arguments greater than 1000000", () => {
+      expect(() => reciprocal(1000001)).to.throw(RangeError, "Argument must be between 1 and 1000000");
+    });
+})
+})
